078b9445563b0f0fdac48985201a79ca84aaecd8
vinay2132 <63947925+vinay2132@users.noreply.github.com>
2020-05-09 14:12:28 +0530

    Update form.php

--- form.php
+++ form.php
@@ -9,14 +9,36 @@ $pswrepeatt= $_POST['pswrepeatt'];
 $s ="select * from form where email = '$email'";
 $result=mysqli_query($con,$s);
 $num=mysqli_num_rows($result);
-if($num==1){
-     echo '<script>alert("Someone already register using this email")</script>';
-     echo "<meta http-equiv='refresh' content='0; url=contact.html' />";
+if($psw == $pswrepeatt ){
+   if($num==1){
+       echo '<script>alert("Someone already register using this email")</script>';
+       echo "<meta http-equiv='refresh' content='0; url=contact.html' />";
+     }
+     else{
+         $vkey =md5(time().$email);
+         $psw = md5($psw);
+         $reg="INSERT Into form (username,email, psw, pswrepeatt,vkey) values('$username','$email','$psw','$pswrepeatt','$vkey')";
+         mysqli_query($con,$reg);
+         echo '<script>alert("New record inserted sucessfully")</script>';
+          if($reg){
+             
+            $to = $email;
+            $subject ="email varification";
+            $message = "<a href='http://localhost/pp/a1/varified.php?vkey=$vkey'>register account</a>";
+            $headers ="from:vinayd.vef@gmail.com\r\n";
+            $headers .="MIME-Version:1.0"."\r\n";
+            $headers .="Content-type:text/html;charset=UTF-8"."\r\n";
+            mail($to,$subject,$message,$headers);
+            header('location:thanks.html');
+          }
+          
+//          echo "<meta http-equiv='refresh' content='0; url=login.html' />";
+     }
 }
 else{
-     $reg="INSERT Into form (username,email, psw,  pswrepeatt) values('$username','$email','$psw','$pswrepeatt')";
-     mysqli_query($con,$reg);
-     echo '<script>alert("New record inserted sucessfully")</script>';
-     echo "<meta http-equiv='refresh' content='0; url=login.html' />";
-}
+     echo'<script>alert("pasword and repeart pasword where not same. Please try again!")</script>';
+     echo "<meta http-equiv='refresh' content='0; url=home.html' />";
+}     
 ?>
+
+
