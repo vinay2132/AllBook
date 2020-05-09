da9c475120f22cc49dca00b1a4dfb0a6ca1f22cf
vinay2132 <63947925+vinay2132@users.noreply.github.com>
2020-05-09 14:15:32 +0530

    Update companyregs.php

--- companyregs.php
+++ companyregs.php
@@ -8,6 +8,7 @@ $psws = $_POST['psws'];
 $problems = $_POST['problems'];
 $companyname = $_POST['companyname'];
 $messag = $_POST['messag'];
+
 $s ="select * from companyregs where emails = '$emails'";
 $result=mysqli_query($con,$s);
 $num=mysqli_num_rows($result);
@@ -16,9 +17,22 @@ if($num==1){
      echo "<meta http-equiv='refresh' content='0; url=companyreg.html' />";
 }
 else{
-     $reg="INSERT Into companyregs (usern,emails, psws,problems,companyname,messag ) values('$usern','$emails','$psws','$problems','$companyname','$messag')";
+    $vkey =md5(time().$emails);
+   // $psws = md5($psws);
+     $reg="INSERT Into companyregs (usern,emails, psws,problems,companyname,messag,vkey ) values('$usern','$emails','$psws','$problems','$companyname','$messag','$vkey')";
      mysqli_query($con,$reg);
-     echo '<script>alert("New record inserted sucessfully")</script>';
-     echo "<meta http-equiv='refresh' content='0; url=serviceprovider.html' />";
+     if($reg){
+        echo '<script>alert("New record inserted sucessfully")</script>';
+        $to = $emails;
+        $subject ="email varification";
+        $message = "<a href='http://localhost/pp/a1/companyvarified.php?vkey=$vkey'>register account</a>";
+        $headers ="from:vinayd.vef@gmail.com\r\n";
+        $headers .="MIME-Version:1.0"."\r\n";
+        $headers .="Content-type:text/html;charset=UTF-8"."\r\n";
+        mail($to,$subject,$message,$headers);
+        header('location:thanks.html');
+     }
+     
+    
 }
 ?>
